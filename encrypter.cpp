3cfab47ae78a217f8a81747e98985ab70874fc4f
Seehwan Yoo <seehwan.yoo@dankook.ac.kr>
2020-09-18 22:36:05 +0900

    Just works, needs code clean up
    	1. now works with input params
    		usage printout
    		encrypter key_path_dir key_value input_file [output_filename]
    	2. now, we work with arbitrary file size
    		buff with malloc
    	3. key_value -1
    		sample random input key_value
    	4. if output_filename has not designated,
    		output_filename = "input_file.enc"
    
    Tested with data0.txt

--- encrypter.cpp
+++ encrypter.cpp
@@ -13,24 +13,32 @@
 #include <openssl/x509.h>
 #include <iostream>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
+#include <fstream>
+#include <streambuf>
 using namespace std;
 
 uint8_t *key;
-uint8_t out[1024];
+uint8_t *out;
 uint8_t dout[1024];
 uint8_t fin[1024];
 size_t fin_len = 1024;
 size_t bufsize = 1024;
+size_t out_len;
+size_t out_buflen;
 size_t dout_len = 1024;
 size_t buf_len = 1024;
 size_t nonce_len = 0;
+std::string input_data;
+char enc_output_filename[256];
 
 RSA *pPrivKey;
 RSA *pPubKey;
 FILE *pFile;
-unsigned char msg[] = "Test RSA Encryption and Decryption";
-unsigned char chiper[256], plain[256], chiper2[256];
+unsigned char cipher[256], plain[256], cipher2[256];
 
 std::string ReplaceAll(std::string str, const std::string &from, const std::string &to)
 {
@@ -98,30 +106,73 @@ uint8_t *RetriveKeyFromString(std::string stf, size_t key_size)
     return key;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 
-    int key_size = 32;
-    size_t out_len = sizeof(out);
-    std::string input_key = "f2 fc e5 0d c7 91 9c d6 07 7e 60 35 3e c9 ab d5 a0 a8 4a 2d 7d a5 07 e8 34 a7 e0 c0 6d ea bc 20";
+	if ((argc != 4) && (argc != 5)) {
+		printf("usage: encrypter key_path_dir key_value input_file output_file\n");
+		printf("note that you should have private.key, public.crt in the key_path_dir\n");
+		printf("note that you should provide 256 bit key stream in string\n");
+		return 0;
+	}
+	struct stat sbuf;
+	char *key_path_dir = argv[1];
+	// no valid key_path_dir
+	if (stat(key_path_dir, &sbuf) == -1) {
+		printf("No valid key_path_dir directory\n\n");
+		printf("usage: encrypter key_path_dir key_value input_file output_file\n");
+		printf("note that you should have private.key, public.crt in the key_path_dir\n");
+		printf("note that you should provide 256 bit key stream in string\n");
 
-    pPrivKey = RetrivePrivKeyFromFile("private.key");
-    pPubKey = RetrivePubKeyFromX509("public.crt");
+		return 0;
+	}
+
+    int key_size = 32;
+	std::string input_key(argv[2]);
+	if (strncmp("-1", argv[2], 2) == 0) {
+		// 256 bit key stream (32bytes)
+		std::string key("f2 fc e5 0d c7 91 9c d6 07 7e 60 35 3e c9 ab d5 a0 a8 4a 2d 7d a5 07 e8 34 a7 e0 c0 6d ea bc 20");
+		printf("default_key_value: %s\n", key.c_str());
+		input_key = key;
+	}
+
+	if  (stat(argv[3], &sbuf) == -1) {
+		printf("Invalid encrypting input file, we use default sample input_data\n\n");
+		printf("usage: encrypter key_path_dir key_value input_file output_file\n");
+		printf("note that you should have private.key, public.crt in the key_path_dir\n");
+		printf("note that you should provide 256 bit key stream in string\n");
+		std::string sample_input_data = "The Common Data Model standard defines a common language for business entities covering, over time, the full range of business processes across \
+    sales, services, marketing, operations, finance, talent, and commerce and for the Customer, People, and Product entities at the core of a company's business processes.";
+
+		input_data = sample_input_data;
+		return 0;
+	}
+	if (argc == 4) {
+		sprintf(enc_output_filename, "%s.enc", argv[3]);
+	}
+
+
+	char Kpriv[256];
+	char Kpub[256];
+	sprintf(Kpriv, "%s/private.key", key_path_dir);
+	sprintf(Kpub, "%s/public.crt", key_path_dir);
+    pPrivKey = RetrivePrivKeyFromFile(Kpriv);
+    pPubKey = RetrivePubKeyFromX509(Kpub);
 
     RSA_private_encrypt(strlen(input_key.c_str()), (const unsigned char*)input_key.c_str(),
-                        chiper, pPrivKey, RSA_PKCS1_PADDING);
+                        cipher, pPrivKey, RSA_PKCS1_PADDING);
 
-    WriteToFS("enc_key",chiper);
-    ReadFromFS("enc_key",chiper2);
+    WriteToFS("enc_key",cipher);
+    ReadFromFS("enc_key",cipher2);
 
-    RSA_public_decrypt(sizeof(chiper2), chiper2, plain, pPubKey, RSA_PKCS1_PADDING);
+    RSA_public_decrypt(sizeof(cipher2), cipher2, plain, pPubKey, RSA_PKCS1_PADDING);
 
     cout << "[DEBUG] Original input_key :\n"
          << input_key << endl;
     cout << "[DEBUG] Post process input_key :\n"
          << plain << endl;
     cout << "[DEBUG] Chiper :\n"
-         << chiper << endl;
+         << cipher << endl;
 
     uint8_t *key = RetriveKeyFromString(
         ReplaceAll(input_key, std::string(" "), std::string("")), 32);
@@ -130,8 +181,14 @@ int main()
         printf("%02x ", key[i]);
     std::cout << std::endl;
 
-    string input_data = "The Common Data Model standard defines a common language for business entities covering, over time, the full range of business processes across \
-    sales, services, marketing, operations, finance, talent, and commerce and for the Customer, People, and Product entities at the core of a company's business processes.";
+	std::ifstream t(argv[3]);
+
+	t.seekg(0, std::ios::end);
+	input_data.reserve(t.tellg());
+	t.seekg(0, std::ios::beg);
+
+	input_data.assign((std::istreambuf_iterator<char>(t)),
+            std::istreambuf_iterator<char>());
 
     Bytef *in = (Bytef *)input_data.c_str();
     cout << "[DEBUG] Input Data : " << endl;
@@ -140,11 +197,15 @@ int main()
     unsigned long in_len = input_data.length();
     unsigned long nCompressedsize = in_len;
 
-    Bytef *pCompressedData = new Bytef[256];
+    Bytef *pCompressedData = new Bytef[in_len];
+	memset(pCompressedData, 0, in_len);
 
     int nResult = compress(pCompressedData, &nCompressedsize, in, in_len);
-    cout << "[DEBUG] Compressed Data : " << endl;
+    cout << "[DEBUG] Compressed Data ("<< nCompressedsize <<"): " << endl;
     cout << pCompressedData << endl;
+
+	out_buflen = (nCompressedsize << 1) + 32;
+	out = new uint8_t[out_buflen];
 
     const EVP_AEAD *aead = EVP_aead_aes_256_gcm();
     static const unsigned char nonce[32] = {0};
@@ -153,12 +214,13 @@ int main()
     EVP_AEAD_CTX_init(&ctx, aead, key, EVP_AEAD_key_length(aead), EVP_AEAD_DEFAULT_TAG_LENGTH, NULL);
     nonce_len = EVP_AEAD_nonce_length(aead);
 
-    EVP_AEAD_CTX_seal(&ctx, out, &out_len, bufsize, nonce, nonce_len, pCompressedData, nCompressedsize, NULL, 0);
+    EVP_AEAD_CTX_seal(&ctx, out, &out_len, out_buflen, nonce, nonce_len, pCompressedData, nCompressedsize, NULL, 0);
 
-    cout << "[DEBUG] Compressed and Encrypted message : " << endl;
+    cout << "[DEBUG] Compressed and Encrypted message ("<< out_len <<"): " << endl;
     cout << out << endl;
 
-    FILE *out_file = fopen("enc_data", "wb");
+    FILE *out_file = NULL;
+	out_file = fopen(enc_output_filename, "wb");
     if (out_file != NULL)
     {
         //printf("%d\n",out_len);
